b150e5c6236d764ebaa88bc845f27196c652ae5a
wkwok16 <wkwok16@punahou.edu>
2014-12-01 12:38:52 -1000

    Commit 5
    - Brute force card checker added
    - Added some comments for the validity checker

--- CreditCard.java
+++ CreditCard.java
@@ -19,8 +19,9 @@ public class CreditCard
     else if(cardNumber.length()%2 == 1)
     {
       evenArraySize = cardNumber.length()/2;
-      oddArraySize = (cardNumber.length()/2)+1;
+      oddArraySize = (cardNumber.length()/2)+1; // Adds one more because odd will have one more value
     }
+    
     // Create new arrays of the correct size
     int[] evenArray = new int[evenArraySize];
     int[] oddArray = new int[oddArraySize];
@@ -29,19 +30,17 @@ public class CreditCard
     int x = 0; // Even
     int y = 0; // Odd
     
-    // Boolean to check if this is the first loop or not, to find end
-    boolean runFirst = true;
-    
     // Move the string into split arrays
     for(int i = 0; i < cardNumber.length(); i++)
     {
       //If it's an even amount of numbers
       if((cardNumber.length()%2) == 0)
       {
+        //Checks if the position is an even or odd number and will add it to the array.
         if((i%2) == 0)
         {
           evenArray[x] = Integer.parseInt(cardNumber.substring(i,i+1));
-          x++;
+          x++; //Increments position that will be input
         }
         else
         {
@@ -63,18 +62,19 @@ public class CreditCard
           y++;
         }
       }
-     // find modulus of i and see if even or odd and also check size of string
     }
     
+    // Doubles all values in the even array, and also checks if the double is greater than 10, and makes it into the correct single digit number
     for(int i = 0; i < evenArray.length; i++)
     {
       evenArray[i] = evenArray[i]*2;
       if(evenArray[i] >= 10)
       {
-        evenArray[i] = evenArray[i]-10+1;
+        evenArray[i] = evenArray[i]-10+1; // Subtracting by 10 and adding 1 will create a fake (but correct) effect of adding the two digits together
       }
     }
     
+    // Adds all values in the arrays together
     for(int i = 0; i < evenArray.length; i++)
     {
       evenArrayTotal += evenArray[i];
@@ -84,27 +84,71 @@ public class CreditCard
       oddArrayTotal += oddArray[i];
     }
     
+    // Returns valid or invalid if the total is divisible by 10 and has a remainder of 0 or something else.
     if(((oddArrayTotal+evenArrayTotal)%10) == 0)
     {
-      System.out.println(oddArraySize + " " + evenArraySize);
       return "It is a valid credit card number";
     }
     else
     {
-      System.out.println(oddArraySize + " " + evenArraySize);
       return "It is not a valid credit card number";
     }     
   }
+  
   public int findType(String cardNumber)
   {
-    return 2;
-    //create an int array of each int value
-    //if first value of the array is 4, return 2;
-    //if first value is 3, and second number is 4 or 7, return 4
-    //if first value is 3, and second number is 6 8, return 5
-    //    if first value is 3, second number is 0, and third number is greater than or equal to 0 or less than 6, then return 5
-    //if first value 5, check if the second value is greater than 0 and less than 6, if it is, return 1.
-    //if the number is 6011, then return 3
-    // else return 0
+    int[] cardArray = new int[cardNumber.length()];
+    
+    for(int i = 0; i < cardNumber.length(); i++)
+    {
+      cardArray[i] = Integer.parseInt(cardNumber.substring(i,i+1));
+    }
+    
+    if(cardArray[0] == 3)
+    {
+      if(cardArray[1] == 4 || cardArray[1] == 7)
+      {
+        return 3;
+      }
+      else if(cardArray[1] == 6 || cardArray[1] == 8)
+      {
+        return 5;
+      }
+      else if(cardArray[1] == 0)
+      {
+        if(cardArray[2] >= 1 && cardArray[2] <= 5)
+        {
+          return 5;
+        }
+      }
+    }
+    
+    else if(cardArray[0] == 4)
+    {
+      return 2;
+    }
+    
+    else if(cardArray[0] == 5)
+    {
+      if(cardArray[1] >= 1 && cardArray[1] <= 5)
+      {
+        return 1;
+      }
+    }
+    
+    else if(cardArray[0] == 6)
+    {
+      if(cardArray[1] == 0)
+      {
+        if(cardArray[2] == 1)
+        {
+          if(cardArray[3] == 1)
+          {
+            return 4;
+          }
+        }
+      }
+    }    
+    return 0;
   }
 }
--- CreditCardLabDriver.java
+++ CreditCardLabDriver.java
@@ -14,11 +14,10 @@ public class CreditCardLabDriver
     while(!done){
       System.out.println("Enter a number to be tested");
       String num = in.next();
-      System.out.println("is "+num+" a valid credit card number? "+ test.verify(num));
+      System.out.println("is "+num+" a valid credit card number?");
+      System.out.println(test.verify(num));
       int typeInt = test.findType(num);
-      System.out.println(typeInt);
       String[] types = {"none", "Master Card", "Visa", "American Express", "Discover", "Diners Club/ Carte Blanche"};
-
       System.out.println("Credit card type is: "+ types[typeInt]);
       System.out.println();
     }
